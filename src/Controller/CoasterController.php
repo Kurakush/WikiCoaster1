240b5a3e788c11f519ef32312d3f37ad8bbe00c8
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-12 14:39:42 +0100

    Rajout d'éléments

---
+++ src/Controller/CoasterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Coaster;
+use App\Form\CoasterType;
+
+
+class CoasterController extends AbstractController
+{
+    #[Route(path: '/coaster/add')]
+    public function add(EntityManagerInterface $em, Request $request):Response
+    {
+        $coaster = new Coaster();
+
+        // création d'un formulaire
+        $form = $this->createForm(CoasterType::class, $coaster);
+
+        // envoi des données
+        $form->handleRequest($request);
+
+        // On s'assure que le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on sauvegarde en base
+            $em->persist($coaster);
+            $em->flush();
+
+            // redirection vers la liste des montagnes russes
+        
+        }
+
+        return $this->render('coaster/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+    
+}
---
+++ src/Form/CoasterType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Coaster;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class CoasterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', options: [
+                'label' => 'Nom de la montagne russe'])
+            ->add('maxSpeed', options: [
+                'label' => 'Vitesse max (km/h)'])
+            ->add('length', options: [
+                'label' => 'Longueur (m)'])
+            ->add('maxHeight', options: [
+                'label' => 'Hauteur max (m)'])
+            ->add('operating', ChoiceType::class, options: [
+                'label' => 'En service ?',
+                'choices' => [ //choix disponibles
+                    'Oui' => true,
+                    'Non' => false,
+ 
+            ],
+                'expanded' => true, //affiche sous forme de boutons radio
+        ])
+    ;
+}
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Coaster::class,
+        ]);
+    }
+}
+
