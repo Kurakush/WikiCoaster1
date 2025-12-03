85b9f8c96b91e57a7e64cec68016ec238c3a6e06
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-12-03 17:41:31 +0100

    début TP4

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,11 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route(path:'/')]
     public function index() : Response
     {
         return $this->render('app/index.html.twig');
